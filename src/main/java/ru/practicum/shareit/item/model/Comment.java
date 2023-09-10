d0778ec9443a4d7716bdc94b800e7c4670af9b59
Merzlenko <merzlenkoad@gmail.com>
2023-09-10 19:35:54 +0300

    first iteration 14 sprint. Column annotation adding.

--- src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -17,12 +17,16 @@ import java.time.LocalDateTime;
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "text")
     private String text;
 
     @ManyToOne
     @JoinColumn(name = "item_id", referencedColumnName = "id")
     private Item item;
+    @Column(name = "author")
     private String authorName;
+    @Column(name = "created")
     private LocalDateTime created;
 }
